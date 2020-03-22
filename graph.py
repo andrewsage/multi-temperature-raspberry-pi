a865753d200178a6ebb7dac6fc067ab57f63941f
Andrew Sage <andrew@asae.co.uk>
2020-03-22 17:52:24 +0000

    Graph now gets data from database instead of CSV

--- graph.py
+++ graph.py
@@ -3,26 +3,38 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import matplotlib.dates as mpl_dates
 import seaborn as sns
+import database_driver
 
 # Handle date time conversions between pandas and matplotlib
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
+dbname = 'sensorsData.db'
+db = database_driver.database(dbname)
+
 sns.set()
 
-df = pd.read_csv('temp_log.csv', parse_dates=['Time'], index_col=['Time'], na_values=['999.99'])
-resampled = df.resample('1T').mean()
+dfFloor = pd.read_sql_query('SELECT timestamp, temperature, humidity FROM DHT_data WHERE sensor=1 ORDER BY timestamp', con, parse_dates=['timestamp'], index_col=['timestamp'])
+dfDesk = pd.read_sql_query('SELECT timestamp, temperature, humidity FROM DHT_data WHERE sensor=1 ORDER BY timestamp', con, parse_dates=['timestamp'], index_col=['timestamp'])
+resampledFloor = dfFloor.resample('1T').mean()
+resampledDesk = dfDesk.resample('1T').mean()
 
-print(df.info())
-print(df)
+print(dfFloor.info())
+print(dfFloor)
 
-print(resampled.info())
-print(resampled)
+print(dfDesk.info())
+print(dfDesk)
+
+print(resampledFloor.info())
+print(resampledFloor)
+
+print(resampledDesk.info())
+print(resampledDesk)
 
 fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
 
-ax1.plot(resampled.index.values, resampled['Temp Floor'], label="floor")
-ax1.plot(resampled.index.values, resampled['Temp Desk'], label="desk")
+ax1.plot(resampledFloor.index.values, resampledFloor['temperature'], label="floor")
+ax1.plot(resampledDesk.index.values, resampledDesk['temperature'], label="desk")
 date_format = mpl_dates.DateFormatter('%Y-%m-%d %H:%M')
 ax1.xaxis_date()
 ax1.xaxis.set_major_formatter(date_format)
@@ -32,8 +44,8 @@ ax1.set_xlabel('Time')
 ax1.set_ylabel('Temperature ($^\circ$C)')
 ax1.set_title('Temperature at floor vs. desk')
 
-ax2.plot(resampled.index.values, resampled['Humidity Floor'], label="floor")
-ax2.plot(resampled.index.values, resampled['Humidity Desk'], label="desk")
+ax2.plot(resampledFloor.index.values, resampledFloor['humidity'], label="floor")
+ax2.plot(resampledDesk.index.values, resampledDesk['humidity'], label="desk")
 ax2.xaxis_date()
 ax2.xaxis.set_major_formatter(date_format)
 
@@ -42,7 +54,7 @@ ax2.set_ylabel('Humidity (%)')
 ax2.set_title('Humidity at floor vs. desk')
 ax2.legend()
 
-ax3.plot(resampled.index.values, resampled['Temp Desk'] - resampled['Temp Floor'], label="Difference")
+ax3.plot(resampledFloor.index.values, resampledFloor['temperature'] - resampledDesk['temperature'], label="Difference")
 date_format = mpl_dates.DateFormatter('%Y-%m-%d %H:%M')
 ax3.xaxis_date()
 ax3.xaxis.set_major_formatter(date_format)
