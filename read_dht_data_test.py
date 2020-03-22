e47323f8b18f14d5ccbc8cbb80b9111c63f5f17e
Andrew Sage <andrew@asae.co.uk>
2020-03-22 17:29:55 +0000

    Sample script for getting last sensor readings.

---
+++ read_dht_data_test.py
@@ -0,0 +1,18 @@
+import database_driver
+
+dbname = 'sensorsData.db'
+db = database_driver.database(dbname)
+
+def getData(sensor):
+	rows = db.select('SELECT * FROM DHT_data WHERE sensor=sensor ORDER BY timestamp DESC LIMIT 1')
+	print(rows)
+	for row in rows:
+		print(row)
+		time = str(row[0])
+		temperature = row[2]
+		humidity = row[3]
+	return time, temperature, humidity
+
+if __name__ == '__main__':
+	getData(1)
+	getData(2)
